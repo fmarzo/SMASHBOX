b219b04123b576ba99c3bc030fec2f6afd6e86e6
fmarzo <fremarzo94@gmail.com>
2024-11-16 16:33:16 +0100

    feat: implemented mqtt connection an reception of triggers from thingsboard

--- Bridge/project/main.py
+++ Bridge/project/main.py
@@ -5,6 +5,7 @@ import serialpy
 import requests
 import serial.tools.list_ports
 from serial.rfc2217 import Serial
+import paho.mqtt.client as mqtt
 
 URL_DEVICE_1 = "http://demo.thingsboard.io/api/v1/o0SzxyiH8fZWK1uQrRh2/telemetry"
 URL_DEVICE_2 = "http://demo.thingsboard.io/api/v1/7QC2PthzFKhkp9AJAW2z/telemetry"
@@ -15,54 +16,52 @@ URL_DEVICE_5 = "http://demo.thingsboard.io/api/v1/1EB9tCjP9BbUM7IYIqAe/telemetry
 #SERIAL_COM = "COM1"
 
 def main ():
-    # show port available and print it
-    print("list of available ports: ")
-    ports = serial.tools.list_ports.comports()
+    #TODO: connect to Acquisition Module
+    count = 0
 
-   #for p in ports:
-   #    print(("Arduino Uno " + "(" + p.name + ")"))
-   #    if (p.description == ("Arduino Uno " + "(" + p.name + ")")):
-   #        ser = serial.Serial(p.name, SERIAL_BAUDRATE)
-   #        break
+    #Main Loop
+    while True:
 
-    #while True:
+        #TODO: Read from Acquisition Module
         #val = ser.read(1)
 
-        ## BOX 1
-        #val = random.randint(0,10)
-        #lock = 0
-        ##lock = random.randint (0,1)
-        #print("BOX_1 " + str(val) + " " + str(lock))
-        #strval =  "{temperature:%d, lock:%d}" % (val,lock)
-        #r = requests.post(URL_DEVICE_1, data=strval)
-#
-        ## BOX 2
-        #val = random.randint(0,10)
-        ##lock = random.randint (0,1)
-        #lock = 0
-        #print("BOX_2 " + str(val) + " " + str(lock))
-        #strval =  "{temperature:%d, lock:%d}" % (val,lock)
-        #r = requests.post(URL_DEVICE_2, data=strval)
-#
-        ## BOX 3
-        #val = random.randint(0,10)
-        ##lock = random.randint (0,1)
-        #lock = 0
-        #print("BOX_3 " + str(val) + " " + str(lock))
-        #strval =  "{temperature:%d, lock:%d}" % (val,lock)
-        #r = requests.post(URL_DEVICE_3, data=strval)
+        #Simulation starts here
+
+        # BOX 1
+        val = random.randint(0,10)
+        lock = random.randint (0,1)
+        print("BOX_1 " + str(val) + " " + str(lock))
+        strval = '{"ID": "1","temperature":%d, "lock": %d}' % (val, lock)
+        r = requests.post(URL_DEVICE_1, data=strval)
+
+        # BOX 2
+        val = random.randint(0,10)
+        lock = random.randint (0,1)
+        print("BOX_2 " + str(val) + " " + str(lock))
+        strval = '{"ID": "2","temperature":%d, "lock": %d}' % (val, lock)
+        r = requests.post(URL_DEVICE_2, data=strval)
+
+        # BOX 3
+        val = random.randint(0,10)
+        lock = random.randint (0,1)
+        print("BOX_3 " + str(val) + " " + str(lock))
+        strval = '{"ID": "3","temperature":%d, "lock": %d}' % (val, lock)
+        r = requests.post(URL_DEVICE_3, data=strval)
 
         # BOX 5
-    val = random.randint(0, 10)
-    lock = 1
-    print("BOX_5: " + str(val) + " " + str(lock))
-    strval = '{"temperature":%d, "lock": %d}' % (val,lock)
-    print (strval)
-    r = requests.post(URL_DEVICE_5, data=strval)
-    sleep(1)
+        val = random.randint(0, 10)
+        lock = random.randint (0,1)
+        print("BOX_5 " + str(val) + " " + str(lock))
+        strval = '{"ID": "5","temperature":%d, "lock": %d}' % (val, lock)
+        r = requests.post(URL_DEVICE_5, data=strval)
+
+        sleep(1)
+
 
 #entry point
 if __name__ == '__main__':
     main()
+
+
 
 
--- Bridge/project/mqtt.py
+++ Bridge/project/mqtt.py
@@ -1,22 +1,17 @@
 import paho.mqtt.client as mqtt
 
 # Define the MQTT broker details
-HIVE = "broker.hivemq.com"
-HIVE_CLOUD = "8146fb450c144928b59c39f764245c06.s1.eu.hivemq.cloud"
 BROKER = "test.mosquitto.org"  # Free MQTT broker
 PORT = 1883                   # Default MQTT port (non-secure)
-HIVE_CLOUD_PORT = 8883
-USER = "fmarzo"
-PASS = "MyToken1"
-TOPIC = "test/topic"          # Topic to publish/subscribe
-CLIENT_ID = "PythonMQTTClient"  # Unique client ID
+TOPIC = "box/alarm"          # Topic to publish/subscribe
 
 # Callback function when the client connects to the broker
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to MQTT Broker!")
         # Subscribe to the topic after connecting
-        #client.subscribe(TOPIC)
+        client.subscribe(TOPIC)
+        print("SUBSCRIBED!")
     else:
         print(f"Failed to connect, return code {rc}")
 
@@ -25,8 +20,7 @@ def on_message(client, userdata, msg):
     print(f"Received message from topic '{msg.topic}': {msg.payload.decode()}")
 
 # Create MQTT client instance
-client = mqtt.Client(client_id=CLIENT_ID)
-#client.username_pw_set(USER, PASS)
+client = mqtt.Client()
 
 # Assign callback functions
 client.on_connect = on_connect
@@ -38,17 +32,10 @@ try:
     client.connect(BROKER, PORT, keepalive=60)
 
     # Start the loop to process network traffic and dispatch callbacks
-    client.loop_start()
+    client.loop_forever()
 
-    client.subscribe("MyTopic")
-
-    # Publish a test message
-    #client.publish(TOPIC, "Hello from Python MQTT Client!")
-    #print(f"Message published to topic '{TOPIC}'")
-    # Keep the script running to listen for incoming messages
-    input("Press Enter to exit...\n")
-    client.loop_stop()
-    #client.disconnect()
-
+except KeyboardInterrupt:
+    print("\nDisconnecting from broker...")
+    client.disconnect()
 except Exception as e:
     print(f"An error occurred: {e}")
