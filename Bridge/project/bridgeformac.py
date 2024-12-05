cadf99864a6580739ea1434f2e24b93ac5c55276
simonebugo <Simone.bugo@gmail.com>
2024-12-05 16:27:06 +0100

    chore: programmed the IOS version of the basic bridge

---
+++ Bridge/project/bridgeformac.py
@@ -0,0 +1,30 @@
+import random
+from time import sleep
+
+import serial
+import requests
+import serial.tools.list_ports
+from serial.rfc2217 import Serial
+
+SERIAL_BAUDRATE = 9600
+SERIAL_COM = "/dev/cu.usbmodem101"
+N_BYTES = 6
+
+def main ():
+    # show port available and print it
+    print("list of available ports: ")
+    ports = serial.tools.list_ports.comports()
+
+    for p in ports:
+        print (p.name + p.description)
+
+    ser = serial.Serial(SERIAL_COM, SERIAL_BAUDRATE)
+
+    while True:
+        val = ser.read(N_BYTES)
+        print (val)
+        sleep(1)
+
+#entry point
+if __name__ == '__main__':
+    main()
