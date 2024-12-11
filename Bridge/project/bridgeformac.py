94bd8a01840606ef6ede46832ab5bea1e7f83857
simonebugo <Simone.bugo@gmail.com>
2024-12-11 18:34:42 +0100

    chore: modified mac prototype of the bridge for lock/unlock

--- Bridge/project/bridgeformac.py
+++ Bridge/project/bridgeformac.py
@@ -5,10 +5,11 @@ import serial
 import requests
 import serial.tools.list_ports
 from serial.rfc2217 import Serial
+import random
 
 SERIAL_BAUDRATE = 9600
 SERIAL_COM = "/dev/cu.usbmodem101"
-N_BYTES = 6
+N_BYTES = 7
 
 def main ():
     # show port available and print it
@@ -19,12 +20,21 @@ def main ():
         print (p.name + p.description)
 
     ser = serial.Serial(SERIAL_COM, SERIAL_BAUDRATE)
-
     while True:
         val = ser.read(N_BYTES)
         print (val)
+
+        if val[5].to_bytes() == b'1':
+            ser.write(b"1")
+            x = 0
+        if val[6].to_bytes() == b'1' and x != 1:
+            user_input = input("central: tipe yes if you want to unlock the box: ")
+            if user_input == "yes":
+                ser.write(b"2")
+                x = 1
         sleep(1)
 
 #entry point
 if __name__ == '__main__':
     main()
+
