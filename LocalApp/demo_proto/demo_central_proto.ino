6bcd1f78eb0de954984e924fa92136cafbdda3a0
fmarzo <fremarzo94@gmail.com>
2025-01-03 00:48:19 +0100

    chore: added demo proto for central application

---
+++ LocalApp/demo_proto/demo_central_proto.ino
@@ -0,0 +1,40 @@
+String ID, pres, temp, hum, infr, lock, open;
+int rx_check = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);           // set pin to input
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  ID = "0";
+  pres = "0";
+  temp = "10";
+  hum = "12";
+  infr = "1";
+  lock = "0";
+  open = "1";
+
+  Serial.print(ID + pres + temp + hum + infr + lock + open);
+
+if  (Serial.available() > 0)
+{
+   if (Serial.read() == 49)
+   {
+
+    digitalWrite(13, HIGH);
+    delay (1000);
+    digitalWrite(13, LOW);
+    delay (1000);
+   }
+ 
+}
+
+delay(1000);
+ 
+
+}
