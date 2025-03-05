405ad8a770a736e1cded12bda40a07150a47c7b0
simonebugo <Simone.bugo@gmail.com>
2025-03-05 09:07:33 +0100

    feat: pushed config.h files

---
+++ LocalApp/central_proto/config.h
@@ -0,0 +1,53 @@
+#ifndef _CONFIG
+#define _CONFIG
+
+/* ---------------------------------------------------------------------------------------------------------------------------------------------------*/
+/* GESTIONE PORTA SERIALE SOFTWARE IN BASE AI DISPOSITIVI UTILIZZATI*/
+
+#if (defined(__AVR__) || defined(ESP8266)) && !defined(__AVR_ATmega2560__)
+// For UNO and others without hardware serial, we must use software serial...
+// pin #2 is IN from sensor (GREEN wire)
+// pin #3 is OUT from arduino  (WHITE wire)
+// Set up the serial port to use softwareserial..
+SoftwareSerial mySerial(2, 3);
+#else
+// On Leonardo/M0/etc, others with hardware serial, use hardware serial!
+// #0 is green wire, #1 is white
+#define mySerial Serial1
+//Se il microcontrollore è AVR (Arduino Uno, Nano, Pro Mini, ecc.) oppure ESP8266, e NON è un Arduino Mega 2560,
+//→ allora viene creata una porta seriale software sui pin 2 e 3 (SoftwareSerial mySerial(2, 3);).
+//Motivo: queste schede hanno solo una porta seriale hardware (Serial) che è usata per la comunicazione USB.
+//Se vogliamo comunicare con un altro dispositivo seriale (come il sensore di impronte), dobbiamo creare una porta seriale software (SoftwareSerial).
+#endif
+/* ---------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+/* ----------------------------------------------------------------- */
+/* DEFINES */
+
+#define ID_SIZE_NUM 128
+#define NO_ERROR 0x00
+#define ID_EXISTING_ERR 0x01
+#define ERR_SYSTEM  0x02;
+#define CENTRAL_CHAR_REGOGNIZE 0x40 /* @ for bridge-central handshake */
+#define CHAR_INFR 0x5F /* _ for notify the infringement*/
+/* -------------------------------------------------------------------*/
+
+
+
+/* -------------------------------------------------------------------*/
+/* GLOBAL VARIABLES */
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+uint8_t id = 0;
+uint8_t ENR = 0;
+int read = 0;
+int init_response = 0;
+int check = 0;
+const int rs=11, en=12, d4=5, d5=6, d6=7, d7=8;
+LiquidCrystal lcd(rs,en,d4,d5,d6,d7);
+int idarr[128]={0};
+/* -------------------------------------------------------------------*/
+
+#endif _CONFIG
