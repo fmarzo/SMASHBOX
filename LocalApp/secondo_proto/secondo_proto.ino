ae90e81a916cff7b1764e5edbc3768462029c400
simonebugo <Simone.bugo@gmail.com>
2025-03-05 10:33:16 +0100

    chore: managed all refactoring problems

--- LocalApp/secondo_proto/secondo_proto.ino
+++ LocalApp/secondo_proto/secondo_proto.ino
@@ -5,15 +5,12 @@
 #include "config.h"
 
 void setup() {
-  // TODO: anche questa è una costante di configurazione. Possiamo settarla
-  Serial.begin(9600);
+  Serial.begin(STANDARD_BOUNDRATE);
   Wire.begin();
-  // TODO: sostituzione macro?
-  pinMode(4, INPUT);
-  pinMode(5, INPUT);
-  pinMode(13, OUTPUT);
-
-  digitalWrite(13, LOW);
+  pinMode(PRESENCE_PIN, INPUT);
+  pinMode(OPEN_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 
   byte deviceID = accel.readDeviceID();
   if (deviceID == 0) 
@@ -57,8 +54,8 @@ void setup() {
       delay(100);
     }
   }
-  
-  char buffer_id [4] = {0};
+  ID = "AAA";
+  /* char buffer_id [4] = {0};
   while(1)
   {
       if (Serial.available() >= 3) 
@@ -67,34 +64,32 @@ void setup() {
         ID.concat(buffer_id);
         break;
       }
-  }
+  } */
 }
 
 
 void loop() {
-  // Lettura di 1 byte dalla Serial 
-  int relock = 0; //variabile che serve per rimettere il lock a 0 (chiuso) una volta che la cassetta è stata aperta e poi ri-chiusa
   int read;
-  String pres;
+  String infr, pres;
 
   if (Serial.available() > 0) 
   {
     read = Serial.read();
     if (read == OPEN_CHAR) 
     {
-      lock = "1";
+      lock = "0";
     } 
     else if (read == INFR_CHAR)
     {
-      lock = "0";
+      lock = "1";
     }
     else 
     {
-      lock = "0";
+      lock = "1";
     }
   }
 
-  if (PRESENCE == LOW) 
+  if (digitalRead(PRESENCE_PIN) == LOW) 
   {
     pres = "1";
   } 
@@ -102,27 +97,29 @@ void loop() {
   {
     pres = "0";
   }
-  if (OPEN == LOW) 
+  if (digitalRead(OPEN_PIN) == LOW) 
   {
     open = "0";
     if (relock == 1)
-    { //ovvero la cassetta è già stata aperta ed è stata chiusa, imposta il lock a 0
-      lock = "0"; //imposto il lock nuovamente a 0 siccome è già stata aperta e chiusa
-      relock = 0; //ri imposto la variabile ausiliaria relock a 0
+    { 
+      /* the box has already been opened (relock == 1) and it has been closed (digitalRead(OPEN_PIN) == LOW), so we set the lock to 1 */
+      lock = "1"; 
+      relock = 0; /* resetting the lock to the deafault value */
     }
   } 
   else
   {
-    if(lock == "1")//la cassetta si può aprire solo se è stata sbloccata con il lock
+    /* the box can be fisical opened only if it has been unlocked throught the sensor (lock == 0) */
+    if(lock == "0")
     { 
         open = "1";
-        relock = 1; //ho aperto la cassetta, imposto la variabile ausiliaria RELOCK a 1
+        relock = 1; /* since i've opened the box we set the relock variable to 1 */
     }
   }
 
   aht.getEvent(&humidity, &temp);
 
-  // Verifica se è la prima esecuzione e salta il controllo iniziale
+  /* if it is the first iteration you can't compare the value with the ones of the previous iteration */
   if (firstRun) 
   {
     firstRun = false;
@@ -133,10 +130,9 @@ void loop() {
   } 
   else 
   {
-    // Aggiorna i dati dell'accelerometro
+    /* updating accellerometer parameters */
     if (accel.update())
     {
-      // Versione in cui invio allarme
       if (abs(X - accel.getX()) > 0.5 || abs(Y - accel.getY()) > 0.5 || abs(Z - accel.getZ()) > 0.5) 
       {
         infr = "1";
@@ -146,16 +142,13 @@ void loop() {
         infr = "0";
       }
 
-      // TODO: sostizione macro
-      digitalWrite(13, HIGH);
+      digitalWrite(LED_BUILTIN, HIGH);
       delay(1000);
-      digitalWrite(13, LOW);
+      digitalWrite(LED_BUILTIN, LOW);
 
       X = accel.getX();
       Y = accel.getY();
       Z = accel.getZ();
-      // TODO: togliamo se non serve
-      //Serial.print("\n");
       Serial.print(ID + pres + round(temp.temperature) + round(humidity.relative_humidity) + infr + lock + open);
     } 
     else 
@@ -168,20 +161,7 @@ void loop() {
     }
   }
 
-  delay(2000);  // Ritardo per evitare letture troppo rapide
+  delay(2000);  /* Delay to prevent overly fast readings */
 }
 
 
-
-// NOTE PER IL REFACTORING:
-
-/* 
-
-1. Decidiamo se scrivere tutto in italiano o in inglese (decidi tu, ma meglio in inglese perchè se carichiamo il progetto su Git abbiamo più audiance)
-2. Scegliamo uno stile per i commenti. Tendenzialmente io userei / *  * / ma va bene anche //. Però decidiamone uno.
-3. Se riusciamo teniamo al massimo 1 riga vuota tra una riga di codice e l'altra.abort
-4. Ti ho messo apposto alcune parentesi che ti eri scordato
-5. Nota sulla definizione delle macro. In realtà ora che ci penso non andrei a modificare direttamente la funzione (ti mando un audio)
-6. Cancelliamo i commenti che non ci aiutano o che non servono più
-7. Cancella anche questa sezione una volta che hai finito
-*/
