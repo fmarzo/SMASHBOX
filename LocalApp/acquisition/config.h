a98d54dc17b1b8814f44a8c47ae24fe1ec9e9919
simonebugo <Simone.bugo@gmail.com>
2025-03-06 09:21:10 +0100

    chore: added config.h to acquisition

---
+++ LocalApp/acquisition/config.h
@@ -0,0 +1,34 @@
+#ifndef _CONFIG
+#define _CONFIG
+
+
+/* NOTIFICATION CHAR */
+
+#define OPEN_CHAR 0x26 /* & for opening lock */
+#define INFR_CHAR 0x2A /* * for infringement lock */
+
+/* SERIAL RATE */
+
+#define STANDARD_BOUNDRATE 9600
+
+/* PINOUT */
+
+#define PRESENCE_PIN 4
+#define OPEN_PIN 5
+
+/* DEFINES */
+
+#define BOX_OPENED 1
+#define BOX_NOT_OPENED 0
+
+#endif _CONFIG
+
+/*
+ERROR LEGEND for Debug
+X1: read device id: failed
+X2: could not find AHT
+X3: write rate: failed
+X4: write range: failed
+X5: start: failed
+X6: update failed
+*/
