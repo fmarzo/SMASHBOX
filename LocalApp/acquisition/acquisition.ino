ce6a798fb2b0f54c95b9a3441048db4f7f5d1eb9
simonebugo <Simone.bugo@gmail.com>
2025-03-15 19:29:40 +0100

    chore: fixed bug for bridge-server communication and optimization of simulation

--- LocalApp/acquisition/acquisition.ino
+++ LocalApp/acquisition/acquisition.ino
@@ -14,6 +14,8 @@ String lock = "1";
 String ID;
 bool firstRun = true; 
 int relock = BOX_NOT_OPENED; /* variable used to check if the box has been fisically opened and closed in order to reset the lock variable to 0 */
+String SIMULATION_STRING = "12154010";
+int SIMULATION = 0;
 
 void setup() {
 
@@ -26,45 +28,57 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW);
 
   byte deviceID = accel.readDeviceID();
-  if (deviceID == 0) 
+
+  if (deviceID == 0 && 
+    !aht.begin() && 
+    !accel.writeRate(ADXL345_RATE_200HZ) && 
+    !accel.writeRange(ADXL345_RANGE_16G) && 
+    !accel.start()) 
   {
-    Serial.print("X1");
-    while (1) 
-    {
-      delay(100);
-    }
-  }
-  if (!aht.begin()) 
-  {
-    Serial.print("X2");
-    while (1)
-      delay(100);
+    SIMULATION = 1;
   }
 
-  if (!accel.writeRate(ADXL345_RATE_200HZ)) 
-  {
-    Serial.print("X3");
-    while (1) 
+  if(!SIMULATION){
+    if (deviceID == 0) 
     {
-      delay(100);
+      Serial.print("X1");
+      while (1) 
+      {
+        delay(100);
+      }
     }
-  }
-
-  if (!accel.writeRange(ADXL345_RANGE_16G)) 
-  {
-    Serial.print("X4");
-    while (1) 
+    if (!aht.begin()) 
     {
-      delay(100);
+      Serial.print("X2");
+      while (1)
+        delay(100);
     }
-  }
 
-  if (!accel.start()) 
-  {
-    Serial.print("X6");
-    while (1) 
+    if (!accel.writeRate(ADXL345_RATE_200HZ)) 
     {
-      delay(100);
+      Serial.print("X3");
+      while (1) 
+      {
+        delay(100);
+      }
+    }
+
+    if (!accel.writeRange(ADXL345_RANGE_16G)) 
+    {
+      Serial.print("X4");
+      while (1) 
+      {
+        delay(100);
+      }
+    }
+
+    if (!accel.start()) 
+    {
+      Serial.print("X5");
+      while (1) 
+      {
+        delay(100);
+      }
     }
   }
   
@@ -81,97 +95,104 @@ void setup() {
 
 
 void loop() {
-  String open = "0";
-  int read;
-  String infr, pres;
+  if(!SIMULATION)
+  {
+    String open = "0";
+    int read;
+    String infr, pres;
 
-  if (Serial.available() > 0) 
-  {
-    read = Serial.read();
-    if (read == OPEN_CHAR) 
+    if (Serial.available() > 0) 
     {
-      lock = "0";
-    } 
-    else if (read == INFR_CHAR)
-    {
-      lock = "1";
-    }
-    else 
-    {
-      lock = "1";
-    }
-  }
-
-  if (digitalRead(PRESENCE_PIN) == LOW) 
-  {
-    pres = "1";
-  } 
-  else 
-  {
-    pres = "0";
-  }
-  if (digitalRead(OPEN_PIN) == LOW) 
-  {
-    open = "0";
-    if (relock == BOX_OPENED)
-    { 
-      /* the box has already been opened (relock == 1) and it has been closed (digitalRead(OPEN_PIN) == LOW), so we set the lock to 1 */
-      lock = "1"; 
-      relock = BOX_NOT_OPENED; /* resetting the lock to the deafault value */
-    }
-  } 
-  else
-  {
-    /* the box can be fisical opened only if it has been unlocked throught the sensor (lock == 0) */
-    if(lock == "0")
-    { 
-        open = "1";
-        relock = BOX_OPENED; /* since i've opened the box we set the relock variable to 1 */
-    }
-  }
-
-  aht.getEvent(&humidity, &temp);
-
-  /* if it is the first iteration you can't compare the value with the ones of the previous iteration */
-  if (firstRun) 
-  {
-    firstRun = false;
-    accel.update();
-    X = accel.getX();
-    Y = accel.getY();
-    Z = accel.getZ();
-  } 
-  else 
-  {
-    /* updating accellerometer parameters */
-    if (accel.update())
-    {
-      if (abs(X - accel.getX()) > 0.5 || abs(Y - accel.getY()) > 0.5 || abs(Z - accel.getZ()) > 0.5) 
+      read = Serial.read();
+      if (read == OPEN_CHAR) 
       {
-        infr = "1";
+        lock = "0";
       } 
+      else if (read == INFR_CHAR)
+      {
+        lock = "1";
+      }
       else 
       {
-        infr = "0";
+        lock = "1";
       }
+    }
 
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(1000);
-      digitalWrite(LED_BUILTIN, LOW);
+    if (digitalRead(PRESENCE_PIN) == LOW) 
+    {
+      pres = "1";
+    } 
+    else 
+    {
+      pres = "0";
+    }
+    if (digitalRead(OPEN_PIN) == LOW) 
+    {
+      open = "0";
+      if (relock == BOX_OPENED)
+      { 
+        /* the box has already been opened (relock == 1) and it has been closed (digitalRead(OPEN_PIN) == LOW), so we set the lock to 1 */
+        lock = "1"; 
+        relock = BOX_NOT_OPENED; /* resetting the lock to the deafault value */
+      }
+    } 
+    else
+    {
+      /* the box can be fisical opened only if it has been unlocked throught the sensor (lock == 0) */
+      if(lock == "0")
+      { 
+          open = "1";
+          relock = BOX_OPENED; /* since i've opened the box we set the relock variable to 1 */
+      }
+    }
 
+    aht.getEvent(&humidity, &temp);
+
+    /* if it is the first iteration you can't compare the value with the ones of the previous iteration */
+    if (firstRun) 
+    {
+      firstRun = false;
+      accel.update();
       X = accel.getX();
       Y = accel.getY();
       Z = accel.getZ();
-      Serial.print(ID + pres + round(temp.temperature) + round(humidity.relative_humidity) + infr + lock + open);
     } 
     else 
     {
-      Serial.print("X6");
-      while (1) 
+      /* updating accellerometer parameters */
+      if (accel.update())
       {
-        delay(100);
+        if (abs(X - accel.getX()) > 0.5 || abs(Y - accel.getY()) > 0.5 || abs(Z - accel.getZ()) > 0.5) 
+        {
+          infr = "1";
+        } 
+        else 
+        {
+          infr = "0";
+        }
+
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(1000);
+        digitalWrite(LED_BUILTIN, LOW);
+
+        X = accel.getX();
+        Y = accel.getY();
+        Z = accel.getZ();
+        Serial.print(ID + pres + round(temp.temperature) + round(humidity.relative_humidity) + infr + lock + open);
+      } 
+      else 
+      {
+        Serial.print("X6");
+        while (1) 
+        {
+          delay(100);
+        }
       }
     }
+  }
+  else
+  {
+    Serial.print(ID + SIMULATION_STRING);
   }
 
   delay(2000);  /* Delay to prevent overly fast readings */
