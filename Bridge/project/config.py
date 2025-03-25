68a8c683608509250f7f626f8c6c454e55310f54
fmarzo <francesco.marzo@datasensing.com>
2025-03-25 12:33:39 +0100

    chore: changes topic path

--- Bridge/project/config.py
+++ Bridge/project/config.py
@@ -42,7 +42,7 @@ DB_ROOT_PATH = "/Customers"
 #MQTT SERVER
 BROKER = "broker.hivemq.com"  # Free MQTT broker
 PORT = 1883                   # Default MQTT port (non-secure)
-TOPIC = "box/alarm"          # Topic to publish/subscribe
+TOPIC = "alarm/box"          # Topic to publish/subscribe
 
 #OPERATING SYSTEM ID
 WINDOWS_OS_ID = "nt"
